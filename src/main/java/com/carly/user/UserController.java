857d9228c0cd39f666ca768126f214d7814184e2
Mkznd <yzundai1@gmail.com>
2023-12-31 21:12:12 +0100

    Created a new Spring Boot project, added User and JWT authentication

---
+++ src/main/java/com/carly/user/UserController.java
@@ -0,0 +1,28 @@
+
+package com.carly.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+}
---
+++ src/main/java/com/carly/user/UserService.java
@@ -0,0 +1,21 @@
+
+package com.carly.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+}
